0670bebdf46d7b65260201bba697ac2c8114bf3d
Abdulloh050507 <142326516+Abdulloh050507@users.noreply.github.com>
2023-09-19 18:59:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -113,11 +113,11 @@ var a = +prompt('Введите первое число')
 var b = +prompt('Введите второе число')
 var c = +prompt('Введите третье число')
 
-if(a > b && a < c ){
+if(a > b && a < c || a < b && a > c){
     alert('Среднее число ' + a)
-}else if(b > a && b < c) {
+}else if(b > a && b < c || b < a && b > c) {
     alert('Среднее число ' + b)  
-}else if(c > a && c < b) {
+}else if(c > a && c < b || c < a && c > b) {
     alert('Среднее число ' + c)
 }else {
     alert('Вы ввели не число')
